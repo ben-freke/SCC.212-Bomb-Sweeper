6f439eab34a7aa62f998feb9fdb05a8987af457a
Ben Freke <b.freke@lancaster.ac.uk>
2014-11-11 20:52:03 +0000

    Inital Upload

---
+++ Bomb Sweeper/src/SmartSquare.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class SmartSquare extends GameSquare
+{
+	private boolean thisSquareHasBomb = false;
+	public static final int MINE_PROBABILITY = 10;
+
+	public SmartSquare(int x, int y, GameBoard board)
+	{
+		super(x, y, "images/blank.png", board);
+
+		Random r = new Random();
+		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
+	}
+
+	public void clicked()
+	{
+		
+			//If this square has a bomb, set the sqaure to show a bomb
+			
+	}
+}
