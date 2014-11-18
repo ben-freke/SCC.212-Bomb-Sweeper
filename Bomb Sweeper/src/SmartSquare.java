4e76ac6d72a96263333f32595bb6f6bac5ad6a8d
Ben Freke <b.freke@lancaster.ac.uk>
2014-11-18 23:39:10 +0000

    Put code into differerent methods, created "gameover" function.

--- Bomb Sweeper/src/SmartSquare.java
+++ Bomb Sweeper/src/SmartSquare.java
@@ -7,7 +7,7 @@ public class SmartSquare extends GameSquare
 {
 	private boolean checkedSquare = false;
 	private boolean thisSquareHasBomb = false;
-	public static final int MINE_PROBABILITY = 5;
+	public static final int MINE_PROBABILITY = 10;
 
 	public SmartSquare(int x, int y, GameBoard board)
 	{
@@ -16,6 +16,20 @@ public class SmartSquare extends GameSquare
 		
 		Random r = new Random();
 		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
+	}
+	
+	public void clicked()
+	{
+		
+			//If this square has a bomb, set the square to show a bomb
+			if (thisSquareHasBomb){
+				blowUp(this);
+			}
+			else{
+				surroundSquares(this);
+			}
+		
+
 	}
 	
 	public boolean testBomb(){
@@ -31,49 +45,32 @@ public class SmartSquare extends GameSquare
 	}
 	
 	public void blowUp(SmartSquare hostSquare){
-		if (!(hostSquare == null)){
-			int x, y;
-			for (x = -1; x<2; x++){
-				for (y = -1; y<2; y++){
-					SmartSquare sq = (SmartSquare)board.getSquareAt((hostSquare.xLocation)+x,(hostSquare.yLocation)+y);
-					if (!(sq == null)){
-
-						if (sq.testBomb()){
-							sq.setImage("src/images/bomb.png");
-						}
-						else{
-							surroundSquares(sq);
-
-						}
-					}
-					blowUp(sq);
-
+		int x = 0;
+		int y = 0;
+		while (true){
+			SmartSquare sq = (SmartSquare)board.getSquareAt((x),(y));
+			if (sq != null){
+				x++;
+				if (sq.testBomb()){
+					sq.setImage("src/images/bomb.png");
 				}
 			}
+			else{
+				x = 0;
+				y++;
+				if ((SmartSquare)board.getSquareAt((x),(y)) == null) break;
+				 
+			}
+
 		}
 	}
 	
-	public void clicked()
-	{
-		
-			//If this square has a bomb, set the square to show a bomb
-			if (thisSquareHasBomb){
-				this.setImage("src/images/bomb.png");
-			}
-			else{
-				surroundSquares(this);
-			}
-		
-
-	}
-	
-	public void surroundSquares(SmartSquare hostSquare){
-		hostSquare.setChecked(true);
+	public int getSurroundBombs(SmartSquare testSquare){
 		int bombsInArea = 0;
 		int x, y;
 		for (x = -1; x<2; x++){
 			for (y = -1; y<2; y++){
-				SmartSquare sq = (SmartSquare)board.getSquareAt((hostSquare.xLocation)+x,(hostSquare.yLocation)+y);
+				SmartSquare sq = (SmartSquare)board.getSquareAt((testSquare.xLocation)+x,(testSquare.yLocation)+y);
 				if (!(sq == null)){
 					if (sq.testBomb()){
 						bombsInArea++;
@@ -81,7 +78,13 @@ public class SmartSquare extends GameSquare
 				}
 			}
 		}
-		
+		return bombsInArea;
+	}
+	
+	public void surroundSquares(SmartSquare hostSquare){
+		hostSquare.setChecked(true);
+		int bombsInArea = getSurroundBombs(hostSquare);
+		int x, y = 0;
 		hostSquare.setImage("src/images/" + bombsInArea + ".png");
 		if (bombsInArea == 0){
 			for (x = -1; x<2; x++){
